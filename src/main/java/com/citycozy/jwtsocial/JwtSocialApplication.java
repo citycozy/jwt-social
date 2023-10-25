aa84fa7ea1ff423cb49fd30a56e53abad1743414
citycozy <citycozyboy@gmail.com>
2023-10-25 14:34:50 +0900

    jwt-social project created

---
+++ src/main/java/com/citycozy/jwtsocial/JwtSocialApplication.java
@@ -0,0 +1,13 @@
+package com.citycozy.jwtsocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSocialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtSocialApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/citycozy/jwtsocial/service/OAUth2UserService.java
@@ -0,0 +1,32 @@
+package com.citycozy.jwtsocial.service;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.List;
+
+public class OAUth2UserService extends DefaultOAuth2UserService {
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        // Role generate
+        List<GrantedAuthority> authorities = AuthorityUtils.createAuthorityList("ROLE_ADMIN");
+
+        // nameAttributeKey
+        String userNameAttributeName = userRequest.getClientRegistration()
+                .getProviderDetails()
+                .getUserInfoEndpoint()
+                .getUserNameAttributeName();
+
+        // DB 로직이 필요하면 추가
+        return new DefaultOAuth2User(authorities, oAuth2User.getAttributes(), userNameAttributeName);
+
+    }
+}
